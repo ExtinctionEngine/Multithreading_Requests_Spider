8ed9ee240b3c868d30a258b6ef0f46b6c0a18966
ExtinctionEngine <qianlima777@gmail.com>
2019-08-23 12:50:25 +0930

    My First Commit from PC

---
+++ main_controller_renrendai.py
@@ -0,0 +1,70 @@
+from housekeeping import *
+import threading
+from queue import Queue
+import spider
+import time
+
+PROJECT_NAME = 'renrendai'
+PROJECT_URL_1 = 'https://dp.nifa.org.cn/HomePage?method=getTargetProjectInfo&sorganation=911101055548793445&stheonlyid=911101055548793445'
+PROJECT_CSV_HEADER = ['URL_key', '项目名称', '项目编号', '项目简介', '项目销售链接', '借款用途', '借款金额（元）', '借款期限', '年化利率',
+                      '预计起息日', '还款方式', '还款方式说明', '项目状态', '募集开始时间', '还款保障措施', '还款来源',
+                      '项目风险评估', '相关费用', '合同模板号', '出借人适当性管理提示', '借款方类型', '姓名', '证件类型',
+                      '证件号码', '工作性质', '其他借款信息', '借款人征信报告情况', '在本平台逾期次数', '在本平台逾期总金额（元）',
+                      '借款人收入及负债情况']
+PROJECT_INITIAL_NUM = 2668603
+PROJECT_MAX_PAGES = 100
+PROJECT_URL_2 = '&sdebtortypeb=01&sfullnames=911101055548793445'
+NUMBER_OF_SPIDERS = 12
+queue = Queue()
+
+create_data_csv(PROJECT_NAME)
+set_csv_header(PROJECT_NAME, PROJECT_CSV_HEADER)
+iterator_set = create_iterator_set(PROJECT_MAX_PAGES)
+
+
+def create_spiders():
+    for _ in range(NUMBER_OF_SPIDERS):
+        t = threading.Thread(target=work, daemon=True)
+        t.start()
+
+
+def work():
+    while True:
+        # time.sleep(2)
+        iterator = queue.get()  # when get() is proceed, it blocks and waits until queue has something to return
+        record = spider.Spider(threading.current_thread().name, PROJECT_URL_1, PROJECT_INITIAL_NUM, iterator, PROJECT_URL_2)
+        if record.get_list()[1] != 'FAIL':
+            with open(PROJECT_NAME + '.csv', 'a', encoding="utf-8") as table:
+                for attribute in record.get_list():
+                    table.write(str(attribute))
+                    table.write('\t')
+                table.write('\n')
+            iterator_set.remove(iterator)
+            print('Queue ' + str(len(iterator_set)) + ' | Crawled ' + str(PROJECT_MAX_PAGES-len(iterator_set)))
+            queue.task_done()
+        else:
+            print('Queue ' + str(len(iterator_set)) + ' | Crawled ' + str(PROJECT_MAX_PAGES - len(iterator_set)))
+            queue.task_done()
+
+
+def create_jobs():
+    for iterator in iterator_set:
+        queue.put(iterator)
+    queue.join()  # to ensure that jobs are done one by one
+    checker()
+
+
+def checker():
+    # queued_iterator = create_iterator_set(PROJECT_MAX_PAGES)
+    if len(iterator_set) > 0:
+        print(str(len(iterator_set)) + ' urls in the queue')
+        create_jobs()
+
+
+start_time = time.time()
+create_spiders()
+checker()
+end_time = time.time()
+print(end_time - start_time)
+
+
---
+++ spider.py
@@ -0,0 +1,108 @@
+import requests
+import re
+from my_fake_useragent import UserAgent
+
+
+class Spider:
+
+    def __init__(self, spider_name, url_part_1, initial_num, iterator, url_part_2=''):
+        self.spider_name = spider_name
+        self.iterator = iterator
+        self.url_key = initial_num + iterator
+        self.url = url_part_1 + str(self.url_key) + url_part_2
+        self.crawl()
+
+    def crawl(self):
+        ua = UserAgent()
+        headers = {'User-Agent': '{}'.format(ua.random())}
+        print(self.spider_name, 'now crawling', self.url_key)
+
+        try:
+            raw_contents = requests.get(self.url, headers=headers).text
+            match_pattern = r'<td(.*?)</td>'
+            level_1_soup_list = re.findall(match_pattern, raw_contents, re.S | re.M)
+            level_2_soup_list = []
+            for level_1_soup in level_1_soup_list:
+                level_2_soup = level_1_soup.split('>')[1]
+                level_2_soup_list.append(level_2_soup)
+
+            project_name = level_2_soup_list[1]
+            project_number = level_2_soup_list[3]
+            project_intro = level_2_soup_list[5]
+            project_link = level_2_soup_list[7].split('\"')[1]  # Special Design
+            project_purpose = level_2_soup_list[9]
+            project_size = level_2_soup_list[11]
+            project_duration = level_2_soup_list[13]
+            project_apr = level_2_soup_list[15]
+            project_repay_start = level_2_soup_list[17]
+            project_repay_method = level_2_soup_list[19].strip()  # Special Design
+            project_repay_details = level_2_soup_list[21]
+            project_status = level_2_soup_list[23].strip()  # Special Design
+            project_raise_start = level_2_soup_list[25]
+            project_guarantee = level_2_soup_list[27]
+            project_repay_source = level_2_soup_list[29]
+            project_risk = level_2_soup_list[31]
+            project_expense = level_2_soup_list[33]
+            project_template_number = level_2_soup_list[35]
+            project_lender_notice = level_2_soup_list[37]
+            project_borrower_type = level_2_soup_list[39].strip()  # Special Design
+            project_borrower_name = level_2_soup_list[43]
+            project_document_type = level_2_soup_list[45].strip()  # Special Design
+            project_document_number = level_2_soup_list[47]
+            project_borrower_job = level_2_soup_list[49]
+            project_borrower_other_info = level_2_soup_list[51]
+            project_borrower_credit = level_2_soup_list[53]
+            project_borrower_default_times = level_2_soup_list[55]
+            project_borrower_default_amounts = level_2_soup_list[57]
+            project_borrower_income_and_debt = level_2_soup_list[59]
+
+            self.list_of_attribute = [self.url_key, project_name, project_number, project_intro, project_link, project_purpose, project_size,
+                                      project_duration, project_apr, project_repay_start, project_repay_method, project_repay_details,
+                                      project_status, project_raise_start, project_guarantee, project_repay_source, project_risk,
+                                      project_expense, project_template_number, project_lender_notice, project_borrower_type, project_borrower_name,
+                                      project_document_type, project_document_number, project_borrower_job, project_borrower_other_info,
+                                      project_borrower_credit, project_borrower_default_times, project_borrower_default_amounts, project_borrower_income_and_debt]
+
+            print(self.spider_name, 'has finished the crawling from', self.url_key)
+
+        except:
+            project_name = "FAIL"
+            project_number = "FAIL"
+            project_intro = "FAIL"
+            project_link = "FAIL"
+            project_purpose = "FAIL"
+            project_size = "FAIL"
+            project_duration = "FAIL"
+            project_apr = "FAIL"
+            project_repay_start = "FAIL"
+            project_repay_method = "FAIL"
+            project_repay_details = "FAIL"
+            project_status = "FAIL"
+            project_raise_start = "FAIL"
+            project_guarantee = "FAIL"
+            project_repay_source = "FAIL"
+            project_risk = "FAIL"
+            project_expense = "FAIL"
+            project_template_number = "FAIL"
+            project_lender_notice = "FAIL"
+            project_borrower_type = "FAIL"
+            project_borrower_name = "FAIL"
+            project_document_type = "FAIL"
+            project_document_number = "FAIL"
+            project_borrower_job = "FAIL"
+            project_borrower_other_info = "FAIL"
+            project_borrower_credit = "FAIL"
+            project_borrower_default_times = "FAIL"
+            project_borrower_default_amounts = "FAIL"
+            project_borrower_income_and_debt = "FAIL"
+
+            self.list_of_attribute = ["FAIL", project_name, project_number, project_intro, project_link, project_purpose, project_size,
+                                      project_duration, project_apr, project_repay_start, project_repay_method, project_repay_details,
+                                      project_status, project_raise_start, project_guarantee, project_repay_source, project_risk,
+                                      project_expense, project_template_number, project_lender_notice, project_borrower_type, project_borrower_name,
+                                      project_document_type, project_document_number, project_borrower_job, project_borrower_other_info,
+                                      project_borrower_credit, project_borrower_default_times, project_borrower_default_amounts, project_borrower_income_and_debt]
+            print(self.spider_name, "has failed and gives", self.url_key, "to another spider")
+
+    def get_list(self):
+        return self.list_of_attribute
