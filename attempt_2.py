8e7c35936974ebec7f1519753cb892fc55472019
ExtinctionEngine <qianlima777@gmail.com>
2019-08-30 15:43:06 +0930

    Regular Updates from PC

--- attempt_2.py
+++ attempt_2.py
@@ -54,5 +54,5 @@ from spider import Spider
 # soup = string.strip()
 # print(soup)
 
-result = Spider('Spider_X', 'https://dp.nifa.org.cn/HomePage?method=getTargetProjectInfo&sorganation=911101055548793445&stheonlyid=911101055548793445', 2668603, 2, '&sdebtortypeb=01&sfullnames=911101055548793445')
+result = Spider('Spider_X', 'https://dp.nifa.org.cn/HomePage?method=getTargetProjectInfo&sorganation=911101055548793445&stheonlyid=911101055548793445', 3708541, 2, '&sdebtortypeb=01&sfullnames=911101055548793445')
 print(result.get_list())
